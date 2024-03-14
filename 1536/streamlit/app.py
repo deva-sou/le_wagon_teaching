8e1376693b03a02b2e403277a8c24664219e6d21
Deva <soudeva974@gmail.com>
2024-03-14 11:33:51 +0100

    add g plot()

--- 1536/streamlit/app.py
+++ 1536/streamlit/app.py
@@ -21,7 +21,7 @@ def main():
     p2 = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
     p3 = Polygon([(2, 0), (3, 0), (3, 1), (2, 1)])
     g = geopandas.GeoSeries([p1, p2, p3])
-    g
+    g.plot()
 
 
 if __name__ == "__main__":
