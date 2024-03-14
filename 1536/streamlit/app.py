ac44d91169ea054da4de27174127ffff16026c84
Deva <soudeva974@gmail.com>
2024-03-14 11:18:41 +0100

    add geopandas

--- 1536/streamlit/app.py
+++ 1536/streamlit/app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import geopandas
+import geopandas
+from shapely.geometry import Polygon
 
 
 def main():
@@ -6,6 +10,19 @@ def main():
 
     # Add your content here
     st.write("Welcome to my Streamlit app!")
+
+    """
+    # My first app
+    Here's our first attempt at using data to create a table:
+    """
+    df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+
+    df
+    p1 = Polygon([(0, 0), (1, 0), (1, 1)])
+    p2 = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
+    p3 = Polygon([(2, 0), (3, 0), (3, 1), (2, 1)])
+    g = geopandas.GeoSeries([p1, p2, p3])
+    g
 
 
 if __name__ == "__main__":
